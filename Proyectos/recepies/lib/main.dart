d95e97a0c75107acb1b1c03f8ae96c2e9c18ac7b
Abima <abimael.santa1@gmail.com>
2024-03-19 19:21:07 -0400

    transition animation

--- Proyectos/recepies/lib/main.dart
+++ Proyectos/recepies/lib/main.dart
@@ -7,10 +7,10 @@ void main() {
 }
 
 class InitialPage extends StatelessWidget {
-  const InitialPage({super.key})
+  const InitialPage({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
@@ -23,5 +23,28 @@ class InitialPage extends StatelessWidget {
       ),
     );
   }
-  
+}
+
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) =>
+        const RecepiesPage(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return child;
+    },
+  );
+}
+
+class RecepiesPage extends StatelessWidget {
+  const RecepiesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Page 2'),
+      ),
+    );
   }
+}
