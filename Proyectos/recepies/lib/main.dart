81ae409ce983bf93d66c67fed92410daeab57534
Abima <abimael.santa1@gmail.com>
2024-03-19 20:31:55 -0400

    Preparacion del WebView para implementarlos en Flutters

--- Proyectos/recepies/lib/main.dart
+++ Proyectos/recepies/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
+//Main aplication Loop
 void main() {
   runApp(const MaterialApp(
     home: InitialPage(),
   ));
 }
 
+//Initial Page (Logo,Botton to navigate to the Recepies)
 class InitialPage extends StatelessWidget {
   const InitialPage({super.key});
 
@@ -18,17 +21,18 @@ class InitialPage extends StatelessWidget {
           onPressed: () {
             Navigator.of(context).push(_createRoute());
           },
-          child: const Text('Go!'),
+          child: const Text("Abi's Recepies"),
         ),
       ),
     );
   }
 }
 
+//Routing Handle Initial to Recepies Page
+
 Route _createRoute() {
   return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) =>
-        const RecepiesPage(),
+    pageBuilder: (context, animation, secondaryAnimation) => RecepiesPage(),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
       const begin = Offset(0.0, 1.0);
       const end = Offset.zero;
@@ -44,15 +48,54 @@ Route _createRoute() {
   );
 }
 
-class RecepiesPage extends StatelessWidget {
-  const RecepiesPage({super.key});
+//Recepies Page, TabNavigation to web_view
+class RecepiesPage extends StatefulWidget {
+  RecepiesPage({super.key});
+
+  @override
+  _RecepiesPageState createState() => _RecepiesPageState();
+}
+
+class _RecepiesPageState extends State<RecepiesPage> {
+  int _currentIndex = 0;
+
+  final List<String> urls = [
+    'http://arecibo.inter.edu/recepies/food',
+    'http://arecibo.inter.edu/recepies/dessert',
+    'http://arecibo.inter.edu/recepies/drink'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text('Page 2'),
+      appBar: AppBar(
+        title: Text('Select a Recepy'),
+      ),
+      body: WebView(
+        initialUrl: urls[_currentIndex],
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fastfood),
+            label: 'Food',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.cake),
+            label: 'Dessert',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_drink),
+            label: 'Drink',
+          ),
+        ],
       ),
     );
   }
