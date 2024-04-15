36d563a7cbfd26910ee62bddaad921a7b97a36ef
Cruzade-ab <abimael.santa1@gmail.com>
2024-04-15 09:12:25 -0400

    tasklist container start

--- CourseExamples/todo_app/lib/components/task_list_page.dart
+++ CourseExamples/todo_app/lib/components/task_list_page.dart
@@ -28,30 +28,58 @@ class _TaskListPage extends State<TaskListPage> {
                   child: Shape(),
                 ),
                 Align(
-                    alignment: Alignment.center,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'assets/images/tasks-list-image.png',
-                          width: 120,
-                          height: 120,
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        height: 107,
+                      ),
+                      Image.asset(
+                        'assets/images/tasks-list-image.png',
+                        width: 120,
+                        height: 120,
+                      ),
+                      const SizedBox(height: 13),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 32),
+                        child: Text(
+                          'Completa tus tareas',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white),
                         ),
-                        const SizedBox(height: 13), 
-                        const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 32),
-                          child: Text(
-                            'Completa tus tareas',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                          ),
-                        )
-                      ],
-                    ),
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      )
+                    ],
                   ),
+                ),
               ],
             ),
-          )
+          ),
+          const Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32),
+                child: Text(
+                  'Tareas',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black),
+                ),
+              ),
+            ],
+          ),
         ],
       )),
       floatingActionButton: FloatingActionButton(
