521f3f6e0445ffdb8fbe8f9b019a9aacd3d42d79
Abima <abimael.santa1@gmail.com>
2024-05-07 20:52:01 -0400

    Added Font and images

--- Proyectos/lista_de_notas/lib/main.dart
+++ Proyectos/lista_de_notas/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 import 'models/note.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +20,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Notas',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        textTheme: GoogleFonts.pacificoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
       home: const NotesScreen(),
     );
   }
@@ -34,8 +40,11 @@ class NotesScreen extends StatefulWidget {
 class _NotesScreenState extends State<NotesScreen> {
   final _noteController = TextEditingController();
   late Box<Note> _notesBox;
+
   void _addNote(Note note) async {
-    await _notesBox.put("MyNote", note);
+    final DateTime now = DateTime.now();
+    final String timestamp = now.toIso8601String();
+    await _notesBox.put(timestamp, note);
   }
 
   @override
@@ -53,49 +62,81 @@ class _NotesScreenState extends State<NotesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notas'),
-      ),
-      body: Column(
+      // appBar: AppBar(
+      //   title: const Text(
+      //     'Sistema de Notas',
+      //     style: TextStyle(),
+      //   ),
+      //   centerTitle: true,
+      //   backgroundColor: Colors.transparent,
+      //   elevation: 0,
+
+      // ),
+      resizeToAvoidBottomInset: false,
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: _noteController,
-              decoration: const InputDecoration(labelText: 'Añadir nota'),
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/NoteBook2.png"),
+                fit: BoxFit.fill,
+              ),
             ),
           ),
-          Expanded(
-            child: ValueListenableBuilder(
-              valueListenable: _notesBox.listenable(),
-              builder: (context, box, _) {
-                if (box.values.isEmpty) {
-                  return const Center(
-                    child: Text("No hay notas aún"),
-                  );
-                }
-                return ListView.builder(
-                  itemCount: box.values.length,
-                  itemBuilder: (context, index) {
-                    Note note = box.getAt(index) as Note;
-                    return ListTile(
-                      title: Text(note.title),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () {
-                          box.deleteAt(index);
-                          setState(() {});
-                        },
-                      ),
-                      onTap: () {
-// Para futuras operaciones como editar
+          Column(
+            children: [
+              const SizedBox(
+                height: 65,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(0),
+                child: Text('Notas Diarias', style: TextStyle(
+    fontFamily: 'Pacifico',
+    fontSize: 24,
+  ),)
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _noteController,
+                  decoration: const InputDecoration(
+                    labelText: 'Write a Note',
+                    icon: Icon(Icons.create_outlined),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ValueListenableBuilder(
+                  valueListenable: _notesBox.listenable(),
+                  builder: (context, box, _) {
+                    // if (box.values.isEmpty) {
+                    //   return const Center(
+                    //     child: Text("No hay notas aún"),
+                    //   );
+                    // }
+                    return ListView.builder(
+                      itemCount: box.values.length,
+                      itemBuilder: (context, index) {
+                        Note note = box.getAt(index) as Note;
+                        return ListTile(
+                          title: Text(note.title),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () {
+                              box.deleteAt(index);
+                            },
+                          ),
+                          onTap: () {
+                            // Para futuras operaciones como editar
+                          },
+                        );
                       },
                     );
                   },
-                );
-              },
-            ),
-          )
+                ),
+              ),
+            ],
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
